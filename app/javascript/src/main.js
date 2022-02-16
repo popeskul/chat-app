78d1b2fc6ac9e65c186ec431bf13dc5e2ac1d832
Pavlo Popeskul <ppopeskul@lohika.com>
2022-02-16 23:59:31 +0200

    feature/init-rails-vue: placed one app

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -1,15 +1,9 @@
 import { createApp } from 'vue'
 import Chat from '@/components/Chat.vue'
-import Message from '@/components/Message.vue'
 
 export default () => {
     document.addEventListener('DOMContentLoaded', () => {
         const app = createApp(Chat)
         app.mount('#app-chat')
-    })
-
-    document.addEventListener('DOMContentLoaded', () => {
-        const app = createApp(Message)
-        app.mount('#app-message')
     })
 }
