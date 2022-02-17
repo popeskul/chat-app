5bb83cbd7dc7f60f610db5c869ff584355714f94
Pavlo Popeskul <peterjj91@gmail.com>
2022-02-17 22:06:14 +0200

    feature/init-rails-vue: removed code that makes no sense

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,8 +14,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec/**/support/**/*.rb')].sort.each { |f| require f }
-Dir[Rails.root.join('spec/**/concerns/**/*.rb')].sort.each { |f| require f }
-Dir[Rails.root.join('spec/**/services/**/*.rb')].sort.each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
